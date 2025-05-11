3120d77191e6694a32d0e110dff19042e4019e70
KrpkTA <112825576+krpkTa@users.noreply.github.com>
2025-05-11 14:52:55 +0300

    Add files via upload

---
+++ MqttServer-new/DeviceData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttBroker
+{
+    public class DeviceData
+    {
+        public string UID { get; set; } = "";
+        public Dictionary<string, List<SensorData>> Sensors { get; set; } = new();
+    }
+}
---
+++ MqttServer-new/Program.cs
@@ -0,0 +1,9 @@
+﻿using MqttBroker;
+
+var broker = new Server();
+await broker.Start();
+
+Console.WriteLine("Press Enter to stop...");
+Console.ReadLine();
+
+await broker.Stop();
